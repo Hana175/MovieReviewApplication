70ae2a7d6d65613a6dd033fe7355203ca625ff86
Hana175 <85843200+Hana175@users.noreply.github.com>
2024-09-08 01:25:52 +0300

    Frontend added with functionalities

--- frontend/movie-app/src/api/axiosconfig.js
+++ frontend/movie-app/src/api/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
+  baseURL: "https://567f-154-176-119-15.ngrok-free.app/",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
